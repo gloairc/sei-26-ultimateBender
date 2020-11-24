0132ed162b7df72ce8e6f0d7e921a11fda914f5e
Gloria C <gloria_cheng7@yahoo.com>
2020-11-24 10:40:48 +0800

    display the 5 element power and name for player and computer

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
 const elements = ["Fire", "Metal", "Wood", "Earth", "Water"]
 
-const createPlayer = (name, hp, d1, d2, d3, d4, d5) => {
-    let player = {
-        playerName: name,
-        playerHp : hp,
-        playerChoice : "Not decided yet",
-        playerDamagingPower: {
-            [elements[0]]: d1,
-            [elements[1]]: d2,
-            [elements[2]]: d3,
-            [elements[3]]: d4,
-            [elements[4]]: d5
+const createPlayer = (name, hp, dFire, dMetal, dWood, dEarth, dWater) => {
+    let avatar = {
+        avatarName: name,
+        avatarHp: hp,
+        avatarChoice: "Not decided yet",
+        avatarDamagingPower: {
+            [elements[0]]: dFire,
+            [elements[1]]: dMetal,
+            [elements[2]]: dWood,
+            [elements[3]]: dEarth,
+            [elements[4]]: dWater
         },
-        playerWeakeningPower: {// 2/3 of damagerPower
-            [elements[0]]: Math.ceil(d1/3*2),
-            [elements[1]]: Math.ceil(d2/3*2),
-            [elements[2]]: Math.ceil(d3/3*2),
-            [elements[3]]: Math.ceil(d4/3*2),
-            [elements[4]]: Math.ceil(d5/3*2)
+        avatarWeakeningPower: {// 2/3 of damagerPower
+            [elements[0]]: Math.ceil(dFire / 3 * 2),
+            [elements[1]]: Math.ceil(dMetal / 3 * 2),
+            [elements[2]]: Math.ceil(dWood / 3 * 2),
+            [elements[3]]: Math.ceil(dEarth / 3 * 2),
+            [elements[4]]: Math.ceil(dWater / 3 * 2)
         },
-        playerHealingPower: {// 1/3 of damagerPower
-            [elements[0]]: Math.ceil(d1/3),
-            [elements[1]]: Math.ceil(d1/3),
-            [elements[2]]: Math.ceil(d1/3),
-            [elements[3]]: Math.ceil(d1/3),
-            [elements[4]]: Math.ceil(d1/3)
+        avatarHealingPower: {// 1/3 of damagerPower
+            [elements[0]]: Math.ceil(dFire / 3),
+            [elements[1]]: Math.ceil(dMetal / 3),
+            [elements[2]]: Math.ceil(dWood / 3),
+            [elements[3]]: Math.ceil(dEarth / 3),
+            [elements[4]]: Math.ceil(dWater / 3)
         }
     }
-    return player;
+    return avatar;
 }
 
 const game = {
     rounds: 1,
     message: "Message at the start of the Match",
-    lockMessage: "",
-    player: createPlayer("Glo", 100, 30, 30, 30, 30, 30),
+    player: createPlayer("Player1", 100, 30, 30, 30, 30, 30),
     computer: createPlayer("tutorial", 100, 30, 30, 30, 30, 30)
-
 };
+
+//if another opponent
+// game.computer = createPlayer("opponentname","hp",dF, dM, dWood, dE, dWater)
 
 //handler
 const showPChoice = (event) => { //display Player Choice
     const pChoicev = $(event.target).text();
-    game.player.playerChoice = pChoicev
+    game.player.avatarChoice = pChoicev
     enableConfirmChoiceBtn();
     render();
 }
 
 const enableConfirmChoiceBtn = () => {
-    if (game.player.playerChoice !== "") {
+    if (game.player.avatarChoice !== "") {
         $("#confirmChoiceBtn").attr("disabled", false);
     }
 }
 
 const disableOptAndConfirmBtn = () => {//disable option & confirm choice btn after confirming
-    // game.lockMessage = "" //only need message here if we are delaying computer's choice & result"
     $(".pOptionBtn").attr("disabled", true);
     $("#confirmChoiceBtn").attr("disabled", true)
 }
 
 const showCChoice = () => { //display C's choice
     let cChoicev = elements[Math.floor(Math.random() * elements.length)] //C random choice
-    game.computer.playerChoice = cChoicev
+    game.computer.avatarChoice = cChoicev
 }
 
 //GAME LOGIC FOR THE ULTIMATE SCISSORS PAPER STONE
@@ -72,7 +72,7 @@ const tieLogic = (playerChoice, computerChoice) => {//message for a tie
 }
 
 const isTie = () => {//returns playerC === cChoice
-    return (game.player.playerChoice === game.computer.playerChoice)
+    return (game.player.avatarChoice === game.computer.avatarChoice)
 }
 
 const destroyPair = { //key is victim to 1st item in object, key destroys 2nd itme
@@ -84,7 +84,7 @@ const destroyPair = { //key is victim to 1st item in object, key destroys 2nd it
 }
 
 const isDestroyPair = () => {
-    return ((destroyPair[game.player.playerChoice][game.computer.playerChoice] === "victimTo") || (destroyPair[game.player.playerChoice][game.computer.playerChoice] === "destroys"))
+    return ((destroyPair[game.player.avatarChoice][game.computer.avatarChoice] === "victimTo") || (destroyPair[game.player.avatarChoice][game.computer.avatarChoice] === "destroys"))
 }
 
 const destroyLogic = (playerChoice, computerChoice) => {//if pC and cC are in a destroy relationship
@@ -98,7 +98,7 @@ const destroyLogic = (playerChoice, computerChoice) => {//if pC and cC are in a 
 }
 
 const isweakenHealPair = () => {
-    return ((weakenHealPair[game.player.playerChoice][game.computer.playerChoice] === "weakens") || (weakenHealPair[game.player.playerChoice][game.computer.playerChoice] === "heals"))
+    return ((weakenHealPair[game.player.avatarChoice][game.computer.avatarChoice] === "weakens") || (weakenHealPair[game.player.avatarChoice][game.computer.avatarChoice] === "heals"))
 }
 
 const weakenHealPair = {//key weakens 1st item in object and heals the 2nd item
@@ -123,30 +123,30 @@ const weakenHealLogic = (playerChoice, computerChoice) => {//if pC and cC are in
 
 const gameLogic = () => {//destroy, tie, endMatch
     if (isTie()) {
-        tieLogic(game.player.playerChoice, game.computer.playerChoice)
+        tieLogic(game.player.avatarChoice, game.computer.avatarChoice)
         console.log("pair is a tie")
     } else if (isDestroyPair()) {
-        destroyLogic(game.player.playerChoice, game.computer.playerChoice)
+        destroyLogic(game.player.avatarChoice, game.computer.avatarChoice)
         console.log("pair is a destroy pair")
     } else {
-        weakenHealLogic(game.player.playerChoice, game.computer.playerChoice)
+        weakenHealLogic(game.player.avatarChoice, game.computer.avatarChoice)
         console.log("pair is a weaken-heal pair")
     }
     render()
 }
 
 const isMatchEnd = () => {
-    return (game.player.playerHp === 0 || game.computer.playerHp === 0);
+    return (game.player.avatarHp === 0 || game.computer.avatarHp === 0);
 }
 
 const endMatch = () => {//when hp=0, win or loss message, disable opt&confirm btn, show nextBtn
-    if (game.playerHp === 0) {
+    if (game.player.avatarHp === 0) {
         game.message = "You lost the round and have been defeated. Try harder next time!";
         showNextBtn();
         disableOptAndConfirmBtn();
         $("#nextRoundBtn").css("visibility", "hidden");
 
-    } else if (game.computer.playerHp === 0) {
+    } else if (game.computer.avatarHp === 0) {
         game.message = "You won the round and defeated your opponent. Congratulations!";
         showNextBtn();
         disableOptAndConfirmBtn();
@@ -156,44 +156,44 @@ const endMatch = () => {//when hp=0, win or loss message, disable opt&confirm bt
 
 //CHANGES TO HEALTHPOINTS - DAMAGE, HARM, HEAL
 const damageIncurredToPlayer = () => {//damage to player, cap at 0
-    game.player.playerHp = game.player.playerHp - game.computer.playerDamagingPower[game.computer.playerChoice]; // player.playerDamagingPower
-    if (game.player.playerHp <= 0) {
-        game.player.playerHp = 0
+    game.player.avatarHp = game.player.avatarHp - game.computer.avatarDamagingPower[game.computer.avatarChoice]; // player.playerDamagingPower
+    if (game.player.avatarHp <= 0) {
+        game.player.avatarHp = 0
     }
 }
 
 const damageIncurredToComputer = () => {//damage to computer, cap at 0
-    game.computer.playerHp = game.computer.playerHp - game.player.playerDamagingPower[game.player.playerChoice];
-    if (game.computer.playerHp <= 0) {
-        game.computer.playerHp = 0
+    game.computer.avatarHp = game.computer.avatarHp - game.player.avatarDamagingPower[game.player.avatarChoice];
+    if (game.computer.avatarHp <= 0) {
+        game.computer.avatarHp = 0
     }
 }
 
 const weakenPlayer = () => {//harm to player, cap at 0
-    game.player.playerHp = game.player.playerHp - game.computer.playerWeakeningPower[game.computer.playerChoice];
-    if (game.player.playerHp <= 0) {
-        game.player.playerHp = 0
+    game.player.avatarHp = game.player.avatarHp - game.computer.avatarWeakeningPower[game.computer.avatarChoice];
+    if (game.player.avatarHp <= 0) {
+        game.player.avatarHp = 0
     }
 }
 
 const weakenComputer = () => {//harm to computer, cap at 0
-    game.computer.playerHp = game.computer.playerHp - game.player.playerWeakeningPower[game.player.playerChoice];
-    if (game.computer.playerHp <= 0) {
-        game.computer.playerHp = 0
+    game.computer.avatarHp = game.computer.avatarHp - game.player.avatarWeakeningPower[game.player.avatarChoice];
+    if (game.computer.avatarHp <= 0) {
+        game.computer.avatarHp = 0
     }
 }
 
 const healPlayer = () => {//add heal to max 100
-    game.player.playerHp = game.player.playerHp + game.computer.playerHealingPower[game.computer.playerChoice];
-    if (game.player.playerHp >= 100) {
-        game.player.playerHp = 100
+    game.player.avatarHp = game.player.avatarHp + game.computer.avatarHealingPower[game.computer.avatarChoice];
+    if (game.player.avatarHp >= 100) {
+        game.player.avatarHp = 100
     }
 }
 
 const healComputer = () => {//add heal to max 100
-    game.computer.playerHp = game.computer.playerHp + game.player.playerHealingPower[game.player.playerChoice];
-    if (game.computer.playerHp >= 100) {
-        game.computer.playerHp = 100
+    game.computer.avatarHp = game.computer.avatarHp + game.player.avatarHealingPower[game.player.avatarChoice];
+    if (game.computer.avatarHp >= 100) {
+        game.computer.avatarHp = 100
     }
 }
 
@@ -218,21 +218,17 @@ const playRound = () => {//click confirmChoice -> showCChoice, gameLogic & rende
     disableOptAndConfirmBtn();
     showCChoice();
     showNextRoundBtn();
-    console.log("i have played round")
     gameLogic();//tie, destroy, weaken/harm
-    console.log("run gameLogic")
     if (isMatchEnd()) {
-        endMatch(game.player.playerChoice, game.computer.playerChoice);
+        endMatch(game.player.avatarChoice, game.computer.avatarChoice);
         render()
-        console.log("isMatchend")
     }
 }
 
 const newRound = () => {//reset the messages & buttons
     // game.message = "You have 10 seconds to make a move";
-    game.lockMessage = "";
-    game.player.playerChoice = "To be decided";
-    game.computer.playerChoice = "To be decided";
+    game.player.avatarChoice = "To be decided";
+    game.computer.avatarChoice = "To be decided";
     game.rounds++;
 
     $(".pOptionBtn").attr("disabled", false);
@@ -263,15 +259,27 @@ const setup = () => {
 };
 
 //draw on screen
-const render = () => {
-    $("#pChoice").text(game.player.playerChoice); //want to add in sound here
-    $("#cChoice").text(game.computer.playerChoice);
+const render = () => { 
     $("#message").text(game.message)
-    // $("#lockMessage").text(game.lockMessage)
-
     $("#roundNum").text(game.rounds);
-    $("#playerHp").text(game.player.playerHp);
-    $("#computerHp").text(game.computer.playerHp);
+    
+    $("#playerName").text(game.player.avatarName);
+    $("#playerHp").text(game.player.avatarHp);
+    $("#pChoice").text(game.player.avatarChoice); //want to add in sound here
+    $("#pFireDamage").text(game.player.avatarDamagingPower[elements[0]]);
+    $("#pMetalDamage").text(game.player.avatarDamagingPower[elements[1]]);
+    $("#pWoodDamage").text(game.player.avatarDamagingPower[elements[2]]);
+    $("#pEarthDamage").text(game.player.avatarDamagingPower[elements[3]]);
+    $("#pWaterDamage").text(game.player.avatarDamagingPower[elements[4]]);
+   
+    $("#computerName").text(game.computer.avatarName);
+    $("#computerHp").text(game.computer.avatarHp);
+    $("#cChoice").text(game.computer.avatarChoice);
+    $("#cFireDamage").text(game.computer.avatarDamagingPower[elements[0]]);
+    $("#cMetalDamage").text(game.computer.avatarDamagingPower[elements[1]]);
+    $("#cWoodDamage").text(game.computer.avatarDamagingPower[elements[2]]);
+    $("#cEarthDamage").text(game.computer.avatarDamagingPower[elements[3]]);
+    $("#cWaterDamage").text(game.computer.avatarDamagingPower[elements[4]]);
 
 }
 
